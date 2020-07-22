17422f8959aafaa21c8c1c65d58f9818d0d48823
Wang <wangtianshuo@myhexin.com>
2020-07-22 23:29:12 +0800

    初始化项目

---
+++ SwiftProduct/SwiftProduct/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftProduct
+//
+//  Created by Jrsoul on 2020/7/22.
+//  Copyright © 2020 Jrsoul. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
